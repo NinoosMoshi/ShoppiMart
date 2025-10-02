abc8100132c8b7efda41d586dd5c59d45382d24d
NinoosMoshi <nalkaton@gmail.com>
2025-10-02 07:57:07 -0500

    add interfaces and specifications

---
+++ Services/Catalog/Catalog/Repositories/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Entities;
+using Catalog.Specifications;
+
+
+namespace Catalog.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Pagination<Product>> GetProducts(CatalogSpecParams specParams);
+        Task<IEnumerable<Product>> GetProductsByName(string productName);
+        Task<IEnumerable<Product>> GetProductsByBrand(string brandName);
+        Task<Product> GetProduct(string productId);
+        Task<Product> CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string productId);
+        Task<ProductBrand> GetBrandByBrandIdAsync(string brandId);
+        Task<ProductType> GetTypeByTypeIdAsync(string typeId);
+    }
+}
